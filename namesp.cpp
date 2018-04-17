a6b8a6f9da8eb046637ad1db80b85bdd8e76b0b0
xiezheng <xiez@suchness.cn>
2018-04-17 10:20:37 +0800

    first commit

---
+++ namesp.cpp
@@ -0,0 +1,18 @@
+/*
+ * namesp.cpp
+ *
+ *  Created on: 2018-4-17
+ *      Author: xiezheng
+ */
+
+#include "namesp.h"
+
+namesp::namesp() {
+	// TODO Auto-generated constructor stub
+
+}
+
+namesp::~namesp() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ namesp.h
@@ -0,0 +1,17 @@
+/*
+ * namesp.h
+ *
+ *  Created on: 2018-4-17
+ *      Author: xiezheng
+ */
+
+#ifndef NAMESP_H_
+#define NAMESP_H_
+
+class namesp {
+public:
+	namesp();
+	virtual ~namesp();
+};
+
+#endif /* NAMESP_H_ */
